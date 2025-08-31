65e067d6ae166f1283034131d1092097677c4d74
Str0keOOOO <786619108@qq.com>
2025-08-31 13:28:23 +0800

    feat:修改字体

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@ onLaunch(() => {
   console.log("App Launch");
   uni.loadFontFace({
     family: 'slidefu-regular',
-    source: 'url("https://gitee.com/Str0keOOOO/slidefu-regular-font/raw/main/slidefu-regular.woff2")',
+    source: 'url("https://cdn.jsdelivr.net/gh/Str0keOOOO/qin-opera-qa@main/src/static/fonts/slidefu-regular.woff2")',
+    global: true, // 可选，定义为全局字体
+    local: true, // <-- 核心：优先使用本地缓存
     success() {
       console.log('Slidefu remote font loaded');
     },
