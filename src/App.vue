9e39d2b4dabb35e0ce0651db2a94a6cab53f3e47
Str0keOOOO <786619108@qq.com>
2025-08-31 18:05:23 +0800

    feat:修改字体

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@ onLaunch(() => {
   console.log("App Launch");
   uni.loadFontFace({
     family: 'slidefu-regular',
-    source: 'url("https://cdn.jsdelivr.net/gh/Str0keOOOO/qin-opera-qa@main/src/static/fonts/slidefu-regular.ttf")',
-    global: true, // 可选，定义为全局字体
-    local: true, // <-- 核心：优先使用本地缓存
+    source: 'url("https://cdn.jsdelivr.net/gh/Str0keOOOO/qin-opera-qa@main/src/static/fonts/slidefu-regular-croped.woff2")',
+    global: true,
+    local: true,
     success() {
       console.log('Slidefu remote font loaded');
     },
